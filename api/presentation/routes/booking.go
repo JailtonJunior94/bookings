7a79d5fc139dfa88ba1bce2bdebbc3c110fcef37
jailton.junior <jailton.junior@iterative.com.br>
2021-05-04 14:52:51 -0300

    Adicionado middleware nas rotas de bookings

--- api/presentation/routes/booking.go
+++ api/presentation/routes/booking.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/jailtonjunior94/bookings/api/infrastructure/ioc"
+	"github.com/jailtonjunior94/bookings/api/presentation/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func AddBookingRouter(router fiber.Router) {
-	router.Get("/bookings", ioc.BookingController.Bookings)
-	router.Post("/bookings", ioc.BookingController.CreateBooking)
+	router.Get("/bookings", middlewares.Protected(), ioc.BookingController.Bookings)
+	router.Post("/bookings", middlewares.Protected(), ioc.BookingController.CreateBooking)
 }
